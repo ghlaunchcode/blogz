abc3e1e37b725230f01afc8921a7f24b1507e73e
pskt <geoff@megaman>
2017-10-19 17:26:26 -0500

    Separate models into models.py (finally)

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
 
 # imports
 from flask import Flask, Markup, request, redirect, url_for, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 #TODO force utc and timezone offset awareness
@@ -16,8 +15,7 @@ from datetime import datetime
 import pytz
 from tzlocal import get_localzone
 
-#from models import db, BlogzUser, BlogzEntry
-
+from models import db, BlogzUser, BlogzEntry
 from gh_slogan import getSlogan
 
 ## ENABLE/DISABLE Debugging ###
@@ -31,43 +29,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #g_app.config['SQLALCHEMY_ECHO' ] = True
 
 #for late-loading (comes from models.py)
-#db.init_app(app)
-db = SQLAlchemy( app )
+db.init_app(app)
+#db = SQLAlchemy( app )
 bcrypt = Bcrypt( app )
-
-# BLOGz User Model
-class BlogzUser( db.Model ):
-    id = db.Column( db.Integer, primary_key = True )
-    handle = db.Column( db.String( 127 ) )
-    pass_hash = db.Column( db.String( 60 ) )
-    email = db.Column( db.String( 255 ) )
-    level = db.Column( db.Integer )
-    
-    def __init__( self, handle, password, email, level ):
-        self.handle = handle
-        self.pass_hash = generate_password_hash(password).decode('utf-8')
-        self.email = email
-        self.level = level
-
-# BLOGz Entry Model
-class BlogzEntry( db.Model ):
-    id = db.Column( db.Integer, primary_key = True )
-    #TODO reference user by id
-    #user = db.Column( db.Integer )
-    user = db.Column( db.String(127) )
-    title = db.Column( db.String( 255 ) )
-    entry = db.Column( db.Text )
-    created = db.Column( db.DateTime )
-    modified = db.Column( db.DateTime )
-    edit_count = db.Column( db.Integer )
-    
-    def __init__( self, user, title, entry ):
-        self.user = user
-        self.title = title
-        self.entry = entry
-        #Stored as UTC, will render in SERVER local zone
-        self.created = self.modified = datetime.utcnow()
-        self.edit_count = 0
 
 
 ghSITE_NAME = "BLOGz"
--- models.py
+++ models.py
@@ -6,7 +6,46 @@
 #
 # 2017, Geoffrey Hadler [for LC101:u2]
 
-from app import db, BlogzUser, BlogzEntry
+#from app import db, BlogzUser, BlogzEntry
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import generate_password_hash
+
+db = SQLAlchemy()
+
+# BLOGz User Model
+class BlogzUser( db.Model ):
+    id = db.Column( db.Integer, primary_key = True )
+    handle = db.Column( db.String( 127 ) )
+    pass_hash = db.Column( db.String( 60 ) )
+    email = db.Column( db.String( 255 ) )
+    level = db.Column( db.Integer )
+    
+    def __init__( self, handle, password, email, level ):
+        self.handle = handle
+        self.pass_hash = generate_password_hash(password).decode('utf-8')
+        self.email = email
+        self.level = level
+
+# BLOGz Entry Model
+class BlogzEntry( db.Model ):
+    id = db.Column( db.Integer, primary_key = True )
+    #TODO reference user by id
+    #user = db.Column( db.Integer )
+    user = db.Column( db.String(127) )
+    title = db.Column( db.String( 255 ) )
+    entry = db.Column( db.Text )
+    created = db.Column( db.DateTime )
+    modified = db.Column( db.DateTime )
+    edit_count = db.Column( db.Integer )
+    
+    def __init__( self, user, title, entry ):
+        self.user = user
+        self.title = title
+        self.entry = entry
+        #Stored as UTC, will render in SERVER local zone
+        self.created = self.modified = datetime.utcnow()
+        self.edit_count = 0
+
 
 
 # Create the tables and a default user
