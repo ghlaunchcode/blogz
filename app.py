98eb63e175b995f456672eb961dd338c36dfe749
pskt <geoff@megaman>
2017-10-18 19:09:14 -0500

    Implement model init script / Implement BlogzUser table / Implement password hashing / Update getdep script / Updated login view / Implement minor error checking on login

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 # imports
 from flask import Flask, Markup, request, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 from gh_slogan import getSlogan
 
@@ -18,31 +19,115 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #g_app.config['SQLALCHEMY_ECHO' ] = True
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
+class BlogzUser( db.Model ):
+    id = db.Column( db.Integer, primary_key = True )
+    handle = db.Column( db.String( 127 ) )
+    pass_hash = db.Column( db.String( 60 ) )
+    email = db.Column( db.String( 255 ) )
+    level = db.Column( db.Integer )
+    
+    def __init__( self, handle, password, email, level ):
+        self.handle = handle
+        self.pass_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.email = email
+        self.level = level
+
+
+ghDEBUG = True
 
 ghSITE_NAME = "BLOGz"
+ghPAGE_HOME = "home"
+ghPAGE_BLOG = "posts"
+
+
+#TODO
+@app.before_request
+def verify_user():
+    ##TODO
+    #if ghDEBUG:
+        #testHash = (bcrypt.generate_password_hash('test').decode('utf-8'))
+        #print (testHash, len(testHash))
+    #if session['logLevel'] > 0:
+        #Allow User
+    #else:
+        #Redirect User
+    #return redirect( "/", 302 )
+    placeholder = 0
 
 # ROUTE: '/' :: Main Site Index
 @app.route( "/" )
 def index( ):
-    return render_template('index.html', ghSite_Name=ghSITE_NAME, ghPage_Title="Home", ghSlogan=getSlogan(), ghUser_Name=request.remote_addr)
+    return render_template('index.html', ghSite_Name=ghSITE_NAME, ghPage_Title=ghPAGE_HOME, ghSlogan=getSlogan(), ghUser_Name=request.remote_addr)
 
 # ROUTE: '/blog' :: Blog View Page
 @app.route( "/blog" )
 def blog( ):
-    return render_template('blog.html', ghSite_Name=ghSITE_NAME, ghPage_Title="Blog Entries", ghSlogan=getSlogan() )
+    return render_template('blog.html', ghSite_Name=ghSITE_NAME, ghPage_Title=ghPAGE_BLOG, ghSlogan=getSlogan() )
 
-@app.route( "/login" )
+@app.route( "/login", methods=['POST'] )
 def login( ):
-    return render_template('login.html', ghSite_Name=ghSITE_NAME, ghPage_Title="Login", ghSlogan=getSlogan() )
+    strErrMsg = ""
+    strUserName = ""
+    if request.method == 'POST':
+        
+        strUserName = request.form['inUserName']
+        strUserPass = request.form['inUserPass']
+        
+        #TODO INPUT VALIDATION
+        
+        if ghDEBUG:
+            print( "QUERY User Table..." )
+
+        rowUserEntry = BlogzUser.query.filter_by( handle = strUserName ).first()
+
+        #TODO: CHECK IF FOUND
+        if rowUserEntry:
+            isValidUser = True
+        else:
+            isValidUser = False
+        
+        if ghDEBUG:
+            print( "Valid User:", isValidUser )
+
+        if isValidUser:
+            if ghDEBUG:
+                print( rowUserEntry )
+                print( "handle:",rowUserEntry.handle,"pass:",rowUserEntry.pass_hash,"email:",rowUserEntry.email,"level:",rowUserEntry.level )
+                
+            if ghDEBUG:
+                print( "Compare password hashes..." )
+            
+            isValidPassword = bcrypt.check_password_hash( rowUserEntry.pass_hash, strUserPass )
+            
+            if ghDEBUG:
+                print( isValidPassword )
+                
+            if isValidPassword:
+                #TODO login 
+                placeholder = 0
+            else:
+                #TODO invalid password message??
+                strErrMsg = "Invalid Password Specified"
+        else:
+            #TODO invalid user message??
+            strErrMsg = "Invalid User Specified"
+        
+    return render_template('login.html', ghSite_Name=ghSITE_NAME, ghPage_Title="Login", ghSlogan=getSlogan(), vErrMsg=strErrMsg, vUserName=strUserName )
 
 @app.route( "/logout" )
 def logout( ):
     #TODO
     return redirect( "/", 302 )
 
-@app.route( "/signup" )
+@app.route( "/signup", methods=['POST'] )
 def signup( ):
+    if request.method == 'POST':
+        strUserName = request.form['inUserName']
+        strUserPass0 = request.form['inUserPass0']
+        strUserPass1 = request.form['inUserPass1']
+        strUserEmail = request.form['inUserEmail']
     return render_template('signup.html', ghSite_Name=ghSITE_NAME, ghPage_Title="User Signup", ghSlogan=getSlogan() )
 
 @app.route( "/newpost" )
---
+++ models.py
@@ -0,0 +1,27 @@
+#models.py
+# BLOGz-specific Models
+# TODO:
+# To init database, run this file:
+# `python models.py`
+#
+# 2017, Geoffrey Hadler [for LC101:u2]
+
+from app import db, BlogzUser
+
+
+
+def initDB():
+    print( "BLOGz MODELS :: INIT" )
+    print( "  + CREATE TABLES" )
+    db.create_all()
+    print( "  + CREATE USER root" )
+    print( "    handle: root\n    pass: root\n    email: root@localhost\n    level: 7" )
+    new_user = BlogzUser( "root", "root", "root@localhost", 7 )
+    db.session.add( new_user )
+    print( "  + COMMIT" )
+    db.session.commit()
+    print( "OK!" )
+
+if __name__ == "__main__":
+    initDB()
+    
