b609a027348db3b1a0aee17781e9cb4f0d2da7ab
pskt <geoff@megaman>
2017-10-21 16:37:38 -0500

    setup psql

--- app.py
+++ app.py
@@ -9,16 +9,23 @@ from flask_bcrypt import Bcrypt
 
 ## ENABLE/DISABLE Debugging ###
 ghDEBUG = True
+isHeroku = True
 ###############################
 
 app = Flask(__name__)
 app.config['DEBUG'] = ghDEBUG
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:3306/blogz'
+
+if isHeroku:
+    #postgresql://sotedygyjbbnwp:19eb3f2d2e21d3c02304912fc04b9dbe51144f5686f5a272a374f33db601b9d3@ec2-23-21-158-253.compute-1.amazonaws.com:5432/d392l7jnvv5tde
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:3306/blogz'    
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sotedygyjbbnwp:19eb3f2d2e21d3c02304912fc04b9dbe51144f5686f5a272a374f33db601b9d3@ec2-23-21-158-253.compute-1.amazonaws.com:5432/d392l7jnvv5tde'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False #cghDEBUG
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-app.secret_key = bcrypt.generate_password_hash("mediumWicked").decode('utf-8')
-valid_session_key = bcrypt.generate_password_hash("uservalid").decode('utf-8')
+app.secret_key = bcrypt.generate_password_hash( "mediumWicked" ).decode('utf-8')
+valid_session_key = bcrypt.generate_password_hash( "uservalid" ).decode('utf-8')
