8a78a85d539316e54940bf77a229fcc2a6a2af74
pskt <geoff@megaman>
2017-10-21 15:38:18 -0500

    update port binding

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """Entry point for BLOGz"""
 # main.py
 
+import os
+
 from app import app
 from routes import *
 
 def main():
-    app.run()
+	port = int(os.environ.get('PORT',5000))
+    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
