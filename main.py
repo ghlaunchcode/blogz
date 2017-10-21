e893bb9f085e5287092c6e09ee6a7c2da3035d1d
pskt <geoff@megaman>
2017-10-21 15:40:03 -0500

    update main.py for spaces

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from app import app
 from routes import *
 
 def main():
-	port = int(os.environ.get('PORT',5000))
+    port = int(os.environ.get('PORT',5000))
     app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
