5a7f1b664aec1254e482d694842fb47ece955464
polarysekt <geoff@gd2>
2017-10-23 10:06:50 -0500

    fix app.py confilct

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 ## ENABLE/DISABLE Debugging ###
-ghDEBUG = True
+ghDEBUG = False
 isHeroku = True
 ###############################
 
